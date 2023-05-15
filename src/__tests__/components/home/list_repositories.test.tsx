d2ccbaea1ced821fc7108a400a623014e299fcce
LuanFabricio <luanlimaleite@hotmail.com>
2023-05-14 23:44:34 -0300

    adicionando testes para a listagem

--- src/__tests__/components/home/list_repositories.test.tsx
+++ src/__tests__/components/home/list_repositories.test.tsx
@@ -1,10 +1,48 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
+import type { Repository } from "@/types/repositories";
 import ListRepositories from "@/components/home/list_repositories";
 
-const url =
-  "https://raw.githubusercontent.com/LuanFabricio/portfolio-data/main/repositories.json";
+const repositories: Repository[] = [
+  {
+    name: "Test",
+    tags: ["Tag1", "Tag2"],
+    link: "example.com",
+    description: "Just a test",
+  },
+];
 
 it("Should render", () => {
-  render(<ListRepositories url={url} />);
+  render(<ListRepositories repositories={repositories} />);
+});
+
+describe("Functions", () => {
+  it("Should show all itens on repositories list", () => {
+    const repositories: Repository[] = [
+      {
+        name: "Test1",
+        tags: ["Tag1", "Tag2"],
+        link: "example1.com",
+        description: "Just a test",
+      },
+      {
+        name: "Test2",
+        tags: ["Tag3", "Tag4"],
+        link: "example2.com",
+        description: "Just a test2",
+      },
+      {
+        name: "Test3",
+        tags: ["Tag5", "Tag6"],
+        link: "example3.com",
+        description: "Just a test3",
+      },
+    ];
+
+    render(<ListRepositories repositories={repositories} />);
+
+    const repositoriesElements = screen.getAllByTestId("card-div");
+
+    expect(repositoriesElements.length).toBe(repositories.length);
+  });
 });
